559a03bdddeb13fd7258990f1f9db6dc1aa0fa7e
Abdul Rahman <abdul.rahman@everest.engineering>
2021-07-07 21:23:20 +0530

    Add simple implemetation of View

--- src/com/rahman/oc/cardgame/view/View.java
+++ src/com/rahman/oc/cardgame/view/View.java
@@ -2,14 +2,42 @@ package com.rahman.oc.cardgame.view;
 
 import com.rahman.oc.cardgame.controller.GameController;
 
+import java.util.Scanner;
+
 public  class View {
-    public void something() {}
-    public void setController(GameController gc) {}
-    public void promptForPlayerName() {}
-    public void promptForFlip() {}
-    public void promptForNewGame() {}
-    public void showPlayerName(int playerIndex, String playerName) {}
-    public void showFaceDownCardForPlayer(int playerIndex, String playerName) {}
-    public void showCardForPlayer(int playerIndex, String playerName, String rank, String suit) {}
-    public void showWinner(String winnerName) {}
+    GameController controller;
+    Scanner keyboard = new Scanner(System.in);
+    public void setController(GameController gc) {
+        this.controller = gc;
+    }
+    public void promptForPlayerName() {
+        String name = keyboard.nextLine();
+        if(name.isEmpty()) {
+            controller.startGame();
+        } else {
+            controller.addPlayer(name);
+        }
+    }
+    public void promptForFlip() {
+        System.out.println("Press enter to reveal cards");
+        keyboard.nextLine();
+        controller.flipCards();
+    }
+    public void promptForNewGame() {
+        System.out.println("Press enter to deal again");
+        keyboard.nextLine();
+        controller.startGame();
+    }
+    public void showPlayerName(int playerIndex, String playerName) {
+        System.out.println("[" + playerIndex + "]["+ playerName + "]");
+    }
+    public void showFaceDownCardForPlayer(int playerIndex, String playerName) {
+        System.out.println("[" + playerName + "][][]");
+    }
+    public void showCardForPlayer(int playerIndex, String playerName, String rank, String suit) {
+        System.out.println("[" + playerName + "]["+ rank + "][" + suit + "]");
+    }
+    public void showWinner(String winnerName) {
+        System.out.println("Winner! " + winnerName);
+    }
 }
