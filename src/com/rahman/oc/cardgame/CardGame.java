8d8c93098006b55ba47757da4bd66a23cbf4e22b
Abdul Rahman <abdul.rahman@everest.engineering>
2021-07-07 23:56:46 +0530

    Add a Swing GUI for the game - Apply Dependency inversion principle

--- src/com/rahman/oc/cardgame/CardGame.java
+++ src/com/rahman/oc/cardgame/CardGame.java
@@ -4,12 +4,18 @@ import com.rahman.oc.cardgame.controller.GameController;
 import com.rahman.oc.cardgame.games.HighCardGameEvaulator;
 import com.rahman.oc.cardgame.model.Deck;
 import com.rahman.oc.cardgame.view.ConsoleView;
+import com.rahman.oc.cardgame.view.GameViewable;
+import com.rahman.oc.cardgame.view.SwingView;
 
 public class CardGame {
 
     GameController controller;
     public CardGame() {
-        this.controller = new GameController(new ConsoleView(), new Deck(), new HighCardGameEvaulator());
+        ConsoleView consoleView = new ConsoleView();
+        SwingView swingView = new SwingView();
+
+        this.controller = new GameController(swingView, new Deck(), new HighCardGameEvaulator());
+        swingView.createAndShowGUI();
     }
 
     public void run() {
